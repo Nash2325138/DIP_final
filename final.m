dce5a181a9c95992741d6d00a694b830d79acd62
劉哲宇 <j2325138@gmail.com>
2017-12-22 17:26:03 +0800

    compare about f1 and f2, handle different condition when retation angle >= 30 degree

--- final.m
+++ final.m
@@ -1,14 +1,15 @@
 close all
 clear
-imr = imread('./pic/boat.tiff');
+imr = imread('./pic/bridge.tiff');
 % imr = rgb2gray(imr);
-% imr = imRotateCrop(imr, 26, 'bilinear');
-imr = imresize(imr, 3, 'bilinear');
+imr = imRotateCrop(imr, 23, 'bilinear');
+% imr = imresize(imr, 4.3, 'bilinear');
 
 delta = 5;
 W = 2;
 T = 2;
-[output, I, B] = interpolation_estimate(imr, delta, W, T);
+show = 1;
+[output, I, B] = interpolation_estimate(imr, delta, W, T, show);
 
 I(1:5), B(1:5)
 output
--- interpolation_estimate.m
+++ interpolation_estimate.m
@@ -1,5 +1,5 @@
-function [output, I, B] = interpolation_estimate(imr, delta, W, T)
-    filter = fspecial('laplacian');
+function [output, I, B] = interpolation_estimate(imr, delta, W, T, show)
+    filter = fspecial('laplacian', 0);
     imf = imfilter(imr, filter);
 
     height = size(imr, 1);
@@ -26,25 +26,47 @@ function [output, I, B] = interpolation_estimate(imr, delta, W, T)
         end
     end
 
-    rowf_sum = rowf_sum ./ height;
-    subplot(3,1,1)
-    plot([2: width] ./ width, abs(rowf_sum(2:end)))
+    if show == 1
+        rowf_sum = rowf_sum ./ height;
+        subplot(3,1,1)
+        plot([2: width] ./ width, abs(rowf_sum(2:end)))
 
-    subplot(3,1,2)
-    temp = fft(mean(imf, 1));
-    plot([2: width] ./ width, abs(temp(2:end)))
+        subplot(3,1,2)
+        temp = fft(mean(imf, 1));
+        plot([2: width] ./ width, abs(temp(2:end)))
 
-    ax = subplot(3,1,3);
-    bar([2: width] ./ width, c(2:end))
-    xlim(ax, [0, 1])
+        ax = subplot(3,1,3);
+        bar([2: width] ./ width, c(2:end))
+        xlim(ax, [0, 1])
+    end
 
     records = peak_detection(c, W, T);
 
     [B, I] = sort(records, 'descend');
     I = I ./ width;
+    B = B ./ height;
 
-    a = I(1);
-    b = I(2);
-    output = [acosd(1 - a), asind(b), 1/a];
+    counting_thres = 0.15;
+    if B(1) >= counting_thres
+        if B(2) >= counting_thres
+            f1 = min(I(1), I(2));
+            f2 = max(I(1), I(2));
+            rotate_estimate = [acosd(1 - f1), asind(f2)];
+            
+            if abs(rotate_estimate(1) - rotate_estimate(2)) > 3
+                rotate_estimate(2) = asind(1 - f2);
+            end
+        else
+            % may result from angle too small
+            % which makes peak f1 too small to be found
+            f2 = I(1);
+            rotate_estimate = [nan, asind(f2)];
+        end    
+        output = [rotate_estimate; 1/I(1), 1-1/I(1)];
+%         output(output > 45) = 0;
+    else
+        output = zeros(2);
+    end
+    
 end
 
