3396e221c019c8da2210211b64071cf23df5d714
劉哲宇 <j2325138@gmail.com>
2017-12-23 21:27:46 +0800

    make tt_rotate.tiff

--- final.m
+++ final.m
@@ -1,6 +1,23 @@
 close all
 clear
-imr = imread('./pic/bridge.tiff');
-
+% imr = imread('./pic/1.tiff');
 % test_rotation(imr);
-test_resizing(imr);
+% test_resizing(imr);
+% forgeryDetection(imr, 64, 32)
+
+tank1 = imread('./pic/tank1.tiff');
+tank2 = imread('./pic/tank2.tiff');
+t1 = imread('./pic/tank1_small.tiff');
+t1 = imrotate(t1, 23, 'bilinear');
+
+x_st = 50;
+y_st = 300;
+for x = 1:size(t1, 1)
+    for y = 1:size(t1, 2)
+        if t1(x, y) > 0
+            tank2(x_st + x, y_st + y) = t1(x, y);
+        end
+    end
+end
+imshow(tank2)
+imwrite(tank2, 'pic/tt_rotate.tiff')
