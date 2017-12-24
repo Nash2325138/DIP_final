3820cc580a600652317479ab070eed3d4f092742
劉哲宇 <j2325138@gmail.com>
2017-12-24 11:38:19 +0800

    new picture, test recording

--- test_resizing.m
+++ test_resizing.m
@@ -1,16 +1,21 @@
-function test_resizing(imr)
+function test_resizing(imr, pausing)
     delta = 5;
     W = 2;
     T = 2;
     show = 1;
     figure('pos',[10 10 800 400])
-    for factor = 0.1:0.1:3.7
+    
+    factors = 0.1:0.1:3.7;
+    record = zeros(5, size(angles, 2));
+    for factor = factors
         imr_resize = imresize(imr, factor, 'bilinear');
         [rotate_estimate, resize_estimate,  I, B] = interpolation_estimate(imr_resize, delta, W, T, show);
         fprintf('Resizing factor by %.2f\n', factor)
         fprintf('Eestimated factor: %.3f, %.3f, %.3f\n', resize_estimate)
         fprintf('Estimated angle: %.3f, %.3f\n\n', rotate_estimate)
-        pause(0.5)
+        record(1:2, angle) = rotate_estimate;
+        record(3:5, angle) = resize_estimate;
+        pause(pausing)
     end
 end
 
--- test_rotation.m
+++ test_rotation.m
@@ -1,16 +1,20 @@
-function test_rotation(imr)
+function record = test_rotation(imr, pausing)
     delta = 5;
     W = 2;
     T = 2;
     show = 1;
     figure('pos',[10 10 800 400])
-    for angle = 1:45
+    angles = 1:45;
+    record = zeros(5, size(angles, 2));
+    for angle = angles
         imr_rotate = imRotateCrop(imr, angle, 'bilinear'); 
         [rotate_estimate, resize_estimate,  I, B] = interpolation_estimate(imr_rotate, delta, W, T, show);
         fprintf('Rotating by %d\n', angle)
         fprintf('Eestimated factor: %.3f, %.3f, %.3f\n', resize_estimate)
         fprintf('Estimated angle: %.3f, %.3f\n\n', rotate_estimate)
-        pause(0.5)
+        record(1:2, angle) = rotate_estimate;
+        record(3:5, angle) = resize_estimate;
+        pause(pausing)
     end
 end
 
